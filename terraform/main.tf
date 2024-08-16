c21ad88b3d2245441f66ad4c535c4ab1a06cbe19
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 02:51:42 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -159,22 +159,4 @@ resource "aws_cloudwatch_log_group" "apicommentlog" {
   name = "/ecs/apicomment-log"
 }
 
-# alrme para limite de cpu
-resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
-  alarm_name                = "high-cpu-utilization"
-  comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/ECS"
-  period                    = "300"
-  statistic                 = "Average"
-  threshold                 = "80"
-  alarm_description         = "aplicação usando 80% do cpu"
 
-  dimensions = {
-    ClusterName = aws_ecs_cluster.api_comment-cluster.name
-    ServiceName = aws_ecs_service.api_comment-service.name
-  }
-
-  alarm_actions = ["arn:aws:sns:us-east-1:927434173120:snsteste.fifo"]
-}
