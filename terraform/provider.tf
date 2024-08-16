f6856192e65aea11a33b70139a5e9c732e856812
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 13:52:05 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,16 @@
-provider "aws" {
-  region = "us-east-1"  # região da cloud
-}
-
-tls = {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" 
+    }
+    tls = {
       source  = "hashicorp/tls"
       version = "~> 2.0"  
     }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"  # Substitua pela sua região
 }
