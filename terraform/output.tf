7a9cf7cef4f7febb8ca287cc2a44e5b0969a8197
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 18:20:53 -0300

    Delete terraform/output.tf

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "ecr_repository_url" {
-  description = "URL do repositório ECR"
-  value       = aws_ecr_repository.api.repository_url
-}
-
-output "load_balancer_dns_name" {
-  description = "Nome DNS do Load Balancer"
-  value       = aws_lb.api.dns_name
-}
