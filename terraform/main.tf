fcff870a5e47030a8d426ff8a32fca4894574739
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 14:42:57 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,8 +50,6 @@ resource "aws_route_table" "api_route" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.api_gateway.id
   }
-
-
 }
 
 # Associar a tabela de rotas à subnet pública apisub_publica
@@ -66,7 +64,7 @@ resource "aws_route_table_association" "publicb_association" {
   route_table_id = aws_route_table.api_route.id
 }
 
-#grupo de segurança
+# Grupo de segurança
 resource "aws_security_group" "securityapi" {
   vpc_id = aws_vpc.apivpc.id
 
@@ -85,12 +83,12 @@ resource "aws_security_group" "securityapi" {
   }
 }
 
-resource "aws_ecr_repository" "api-comment" {
-name = var.ecr_repository_name
+# Repositório ECR
+resource "aws_ecr_repository" "api_comment" {
+  name = var.ecr_repository_name
 }
 
-
-#Application Load Balancer (ALB)
+# Application Load Balancer (ALB)
 resource "aws_lb" "api_alb" {
   name               = "api-alb"
   internal           = false
@@ -98,14 +96,15 @@ resource "aws_lb" "api_alb" {
   security_groups    = [aws_security_group.securityapi.id]
   subnets            = [
     aws_subnet.apisub_publica.id,
-    aws_subnet.apisub_publicb.id]
+    aws_subnet.apisub_publicb.id
+  ]
 
   enable_deletion_protection = false
   idle_timeout               = 60
   drop_invalid_header_fields = true
 }
 
-# listener para o ALB
+# Listener para o ALB
 resource "aws_lb_listener" "api_listener" {
   load_balancer_arn = aws_lb.api_alb.arn
   port              = 80
@@ -121,37 +120,9 @@ resource "aws_lb_listener" "api_listener" {
   }
 }
 
-
-
 # Configuração do Cluster ECS
-resource "aws_ecs_cluster" "api_comment-cluster" {
+resource "aws_ecs_cluster" "api_comment_cluster" {
   name = var.ecs_cluster_name
-}
-
-
-resource "aws_ecs_service" "api_comment-service" {
-  name            = "api_comment-service"
-  cluster         = aws_ecs_cluster.api_comment-cluster.id
-  task_definition = aws_ecs_task_definition.api-comment-task.arn
-  desired_count   = 1  
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.apisub_publica.id]
-    security_groups  = [aws_security_group.securityapi.id]
-    assign_public_ip = true
-  }
-
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.api_target_group.arn
-    container_name   = "api_comment-service"
-    container_port   = 3000
-  }
-
-  deployment_controller {
-    type = "ECS"
-  }
 }
 
 # Criar um Target Group
@@ -161,7 +132,7 @@ resource "aws_lb_target_group" "api_target_group" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.apivpc.id
 
-target_type = "ip"
+  target_type = "ip"
 
   health_check {
     path                = "/"
@@ -172,43 +143,20 @@ target_type = "ip"
   }
 }
 
-# Role e Policy para ECS
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role      = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 # Configuração da Task Definition ECS
-resource "aws_ecs_task_definition" "api-comment-task" {
+resource "aws_ecs_task_definition" "api_comment_task" {
   family                   = "api-comment-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
 
-execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "api_comment-service"
-      image     = "${aws_ecr_repository.api-comment.repository_url}:latest"
+      name      = "api_comment_service"
+      image     = "${aws_ecr_repository.api_comment.repository_url}:latest"
       cpu       = 256
       memory    = 512
       essential = true
@@ -236,8 +184,27 @@ resource "aws_cloudwatch_log_group" "apicomment_logs" {
   name = "/ecs/api-comment"
 }
 
-resource "aws_cloudwatch_log_group" "apicommentlog" {
-  name = "/ecs/apicomment-log"
+# Serviço ECS
+resource "aws_ecs_service" "api_comment_service" {
+  name            = "api_comment_service"
+  cluster         = aws_ecs_cluster.api_comment_cluster.id
+  task_definition = aws_ecs_task_definition.api_comment_task.arn
+  desired_count   = 1  
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.apisub_publica.id]
+    security_groups  = [aws_security_group.securityapi.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api_target_group.arn
+    container_name   = "api_comment_service"
+    container_port   = 3000
+  }
+
+  deployment_controller {
+    type = "ECS"
+  }
 }
-
-
