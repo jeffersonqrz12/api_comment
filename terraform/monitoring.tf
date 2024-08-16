3934c71b2850e33bec750e3ebeb40fb4d7680e34
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 15:26:49 -0300

    Update monitoring.tf

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -10,8 +10,8 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   alarm_description         = "aplicação utilizando 80 por cento do cpu"
 
   dimensions = {
-    ClusterName = aws_ecs_cluster.api_comment-cluster.name
-    ServiceName = aws_ecs_service.api_comment-service.name
+    ClusterName = aws_ecs_cluster.api_comment_cluster.name
+    ServiceName = aws_ecs_service.api_comment_service.name
   }
 
   alarm_actions = ["arn:aws:sns:us-east-1:927420173806:teste.fifo"]
