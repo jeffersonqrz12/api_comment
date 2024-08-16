12c8bf3cd00cebba13248ab804fd404ccfb20fe4
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 16:02:29 -0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,9 @@
 output "ecr_repository_url" {
-  value = aws_ecr_repository.api-comment.repository_url
+  description = "URL do repositório ECR"
+  value       = aws_ecr_repository.api.repository_url
+}
+
+output "load_balancer_dns_name" {
+  description = "Nome DNS do Load Balancer"
+  value       = aws_lb.api.dns_name
 }
