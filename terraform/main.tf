35c7a235fe44c67902f1b7e0bbe79151468fa4f0
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 15:17:28 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,30 +100,6 @@ resource "aws_ecs_cluster" "api_comment_cluster" {
 }
 
 
-resource "aws_ecs_service" "api_comment_service" {
-  name            = "api_comment_service"
-  cluster         = aws_ecs_cluster.api_comment_cluster.id
-  task_definition = aws_ecs_task_definition.api_comment_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.apisub_publica.id]
-    security_groups  = [aws_security_group.securityapi.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.api_target_group.arn
-    container_name   = "api_comment_service"
-    container_port   = 5000
-  }
-
-  deployment_controller {
-    type = "ECS"
-  }
-}
-
 resource "aws_lb_target_group" "api_target_group" {
   name     = "api-target-group"
   port     = 5000
@@ -175,7 +151,7 @@ resource "aws_ecs_task_definition" "api_comment_task" {
 
   container_definitions = jsonencode([
     {
-      name      = "api_comment_service"
+      name      = "api_comment-service"
       image     = "${aws_ecr_repository.api_comment.repository_url}:latest"
       cpu       = 256
       memory    = 512
