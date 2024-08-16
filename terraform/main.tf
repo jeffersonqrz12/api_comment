670b6aac0b62a8f231f2bd733eed55b62a0ce79e
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-16 10:14:46 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -201,6 +201,8 @@ resource "aws_ecs_task_definition" "api-comment-task" {
   cpu                      = "256"
   memory                   = "512"
 
+execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([
     {
       name      = "apicomment-container"
