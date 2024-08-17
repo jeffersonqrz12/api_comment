6bf295d87ce764d5678405071c72f2b0335b6af5
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-17 09:34:57 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,10 @@
+locals{
+  common_tags = {
+  Name = Terraform_ECS_Lab"
+
 resource "aws_vpc" "apivpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = { local.common_tags
+  tags =  local.common_tags
 }
