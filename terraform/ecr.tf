eb53c4db7755af73c7638c4bfb10d46cf9a5eb77
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-19 20:49:19 -0300

    Delete terraform directory

--- terraform/ecr.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "api-comment-repo" {
-  name = "api-comment-repo"
-}
--- terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repository_api_comment_repo.endpoint{
-  value = aws_ecr_repository.api_comment_repo.repository_url
-}
