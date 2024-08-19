c202e311cbcf6879001c44f16ff6fa62a321eeb8
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-19 20:51:41 -0300

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
+
+output "alb_url" {
+  value = module.ecs.alb_url
+}
