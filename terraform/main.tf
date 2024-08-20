7c49fa04020fd635de7902d4befa9d3462d17a24
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2024-08-20 09:48:36 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,16 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./modules/vpc/main.tf"
 }
 
 module "ecr" {
-  source          = "./modules/ecr"
+  source          = "./modules/ecr/main.tf"
   repository_name = "my-app-repo"
 }
 
 module "ecs" {
-  source              = "./modules/ecs"
+  source              = "./modules/ecs/main.tf"
   ecr_repository_url  = module.ecr.repository_url
   vpc_id              = module.vpc.vpc_id
   subnet_id           = module.vpc.public_subnet_id
